f08b9ab8d1684ee57b0d8cc55964349a426d941e
Yoon <xexe940116@gmail.com>
2022-02-14 23:55:52 +0900

    next auth 추기ㅏ

--- frontend/pages/page.tsx
+++ frontend/pages/page.tsx
@@ -1,17 +1,22 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
+import { useSession, signIn, signOut } from "next-auth/react"
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>Page 2</title>
-      </Head>
-      <div>
-        This is page 2
-      </div>
-    </div>
-  )
+  const { data: session } = useSession()
+    if (session) {
+      return (
+        <>
+          Signed in as {session.user?.email} <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      )
+    }
+    return (
+      <>
+        Not signed in <br />
+        <button onClick={() => signIn()}>Sign in</button>
+      </>
+    )
 }
 
 export default Home
