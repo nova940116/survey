530814c8f171b63d0ec73bea5eee04145a49873e
Yoon <xexe940116@gmail.com>
2022-02-14 21:03:05 +0900

    first commit

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Page 1</title>
+      </Head>
+      <div>
+        This is page 1
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/pages/page.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Page 2</title>
+      </Head>
+      <div>
+        This is page 2
+      </div>
+    </div>
+  )
+}
+
+export default Home
