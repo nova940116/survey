6c5df3f00cc48efe5908982b9fff2b6f6b3e176f
Yoon <xexe940116@gmail.com>
2022-02-15 21:45:20 +0900

    add to secret

--- frontend/pages/page.tsx
+++ frontend/pages/page.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
     return (
       <>
         Not signed in <br />
-        <button onClick={() => signIn()}>Sign in</button>
+        <button onClick={() => signIn('google')}>Sign in</button>
       </>
     )
 }
