686692d6993f357187e1a6ce553425ea3fcec2f1
Nova <nova940116@gmail.com>
2022-02-27 07:53:01 +0900

    survey page add

--- backend/handler.js
+++ backend/handler.js
@@ -3,8 +3,7 @@ const AWS = require("aws-sdk")
 const dynamo = new AWS.DynamoDB.DocumentClient()
 
 module.exports.survey = async (event) => {
-  let body, request, response
-  let requestJSON = typeof event.body !== 'object' ? JSON.parse(event.body) : event.body
+  let body, request, requestJSON, response
   let statusCode = 200    
   const headers = {
     'Access-Control-Allow-Origin': 'https://survey.novauniverse.me',
@@ -14,9 +13,18 @@ module.exports.survey = async (event) => {
     'Content-Type': 'application/json'
   }
   try {
-    switch (event.routeKey) {   
+    switch (event.routeKey) { 
+      case "GET /{name}":
+        request = {
+          TableName: "survey",
+          Key: {
+            name: event.pathParameters.name
+          }         
+        }         
+        body = await dynamo.get(request).promise()
+        break 
       case "POST /create":
-      
+        requestJSON = JSON.parse(event.body)
         //--- dynamoDB
         
         request = {
@@ -36,7 +44,7 @@ module.exports.survey = async (event) => {
     }
   } catch (err) {
     statusCode = 400
-    body = [err.message, requestJSON]
+    body = [err.message, event.pathParameters.name]
   } finally {
     body = JSON.stringify(body)
   }  
--- frontend/pages/[name].tsx
+++ frontend/pages/[name].tsx
@@ -1,7 +1,27 @@
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import SERVER_URL from '../survey.config'
 
 const Read: NextPage = () => {
-  return <div></div>
+  const router = useRouter()  
+  const [survey, setSurvey] = useState<any>({})
+  useEffect(() => {
+    (async () => {
+      if(router.query.name) {
+        const survey = await fetch(`${SERVER_URL}/chicken`).then(r => r.json())
+        console.log(survey.Item, router.query.name, '@rq')
+        setSurvey(survey.Item)
+      }
+    })()
+  }, [router.query.name])
+  return (
+    <div className="flex justify-center">
+      <div className="w-full sm:w-2/4 p-5 flex justify-center flex-col">
+        <h1>{survey.title}</h1>
+      </div>
+    </div>
+  )
 }
 
 export default Read
